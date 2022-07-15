10859657d37bb2506aa7d1a90d775db47369368c
Ryan Schachte <code@ryan-schachte.com>
2022-07-14 23:27:48 -0700

    Script update

--- generate-hls.sh
+++ generate-hls.sh
@@ -8,11 +8,15 @@ mkdir hls
 
 echo "Generating HLS manifest"
 ffmpeg -i test5.mkv \
- -master_pl_name master.m3u8 \
- -g 30 \
- -hls_time 2 \
- -hls_list_size 0 \
- -f hls \
- -map 0:v \
- -map 0:a \
- hls/kevin.m3u8
+-master_pl_name master.m3u8 \
+-g 30 \
+-hls_time 2 \
+-hls_list_size 0 \
+-f hls \
+-map 0:v \
+-map 0:a \
+hls/segment_.m3u8
+
+echo "Playing video"
+ffplay hls/master.m3u8
+
