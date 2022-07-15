3bfeabaaa9759a14b1a24e8414811b39c53c445d
Ryan Schachte <code@ryan-schachte.com>
2022-07-14 23:08:03 -0700

    Updating manifest generation command

--- generate-hls.sh
+++ generate-hls.sh
@@ -1,3 +1,13 @@
 rm -rf hls
 mkdir hls
-ffmpeg -re -i test5.mkv -map 0:v -map 0:a -f hls ./hls/master.m3u8
+#ffmpeg -re -i test5.mkv -map 0:v -map 0:a -f hls ./hls/master.m3u8
+
+ffmpeg -i test5.mkv \
+ -master_pl_name master.m3u8 \
+ -g 30 \
+ -hls_time 2 \
+ -hls_list_size 0 \
+ -f hls \
+ -map 0:v \
+ -map 0:a \
+ hls/kevin.m3u8
